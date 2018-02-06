5702f5f481847036d4cd1e0a74c8f936c9c4307c
hansien <hansien@live.com>
2018-02-06 23:51:10 +0800

    angular material build 1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Router } from '@angular/router';
 
-import 'hammerjs';
-
 import { ManModule } from './controller/sys_manage/man.module'
 import { IndexModule } from './controller/index/index.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { PageNotFoundComponent} from './not-found.component'
-import {PizzaPartyAppModule} from './pizza-party-app.module'
+
+import { PizzaPartyAppModule } from './pizza-party-app.module'
+import { MaterialModule } from './material.module'
 @NgModule({
   imports: [
     BrowserModule,
@@ -19,6 +19,7 @@ import {PizzaPartyAppModule} from './pizza-party-app.module'
     AppRoutingModule,
 
     PizzaPartyAppModule,
+    MaterialModule,
   ],
   declarations: [
     AppComponent,
