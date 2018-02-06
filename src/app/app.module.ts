4f15e46816298a4a355033edf6674ddfc47be2d1
hansien <hansien@live.com>
2018-02-06 23:06:02 +0800

    add Angular Material and Angular CDK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Router } from '@angular/router';
 
+import 'hammerjs';
+
 import { ManModule } from './controller/sys_manage/man.module'
 import { IndexModule } from './controller/index/index.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { PageNotFoundComponent} from './not-found.component'
-
+import {PizzaPartyAppModule} from './pizza-party-app.module'
 @NgModule({
   imports: [
     BrowserModule,
     ManModule,
     IndexModule,
     AppRoutingModule,
+
+    PizzaPartyAppModule,
   ],
   declarations: [
     AppComponent,
---
+++ src/app/pizza-party-app.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [NoopAnimationsModule],
+})
+export class PizzaPartyAppModule { }
