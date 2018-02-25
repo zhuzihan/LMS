835bcf834297f178f94281eb289dfe39b2011d0a
yuchen95 <lyc8801@163.com>
2018-02-25 22:42:39 +0800

    add data manage service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 // import { ReactiveFormsModule } from '@angular/forms';
 // import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { Router } from '@angular/router';
 
-import { ManModule } from './controller/sys_manage/man.module'
+import { ManModule } from './controller/sys_manage/man.module';
 import { IndexModule } from './controller/index/index.module';
-import { ModelDataService } from './controller/sys_manage/model-data.service'
+
 import { AppRoutingModule } from './app-routing.module';
 
-import { PageNotFoundComponent} from './not-found.component'
+import { PageNotFoundComponent } from './not-found.component';
 
-import { PizzaPartyAppModule } from './pizza-party-app.module'
-import { MaterialModule } from './material.module'
+import { PizzaPartyAppModule } from './pizza-party-app.module';
+import { MaterialModule } from './material.module';
 import './polyfill';
 
-
+// Import Service
+import { DataManageService } from './service/data-manage.service';
+import { ModelDataService } from './controller/sys_manage/model-data.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     // ReactiveFormsModule,
     // FormsModule,
+    HttpModule,
 
     ManModule,
     IndexModule,
@@ -37,7 +41,7 @@ import './polyfill';
     AppComponent,
     PageNotFoundComponent,
   ],
-  providers: [ModelDataService],
+  providers: [ModelDataService, DataManageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
