da98ff7dacb259eb6637f6d71aa7b77e06374fcb
hansien <hansien@live.com>
2018-03-01 22:59:48 +0800

    数组数据调整模拟真实

--- src/app/model/data-model.ts
+++ src/app/model/data-model.ts
@@ -323,7 +323,7 @@ table_1.cells = {
 
 export const array_cell_1_1 = new ArrayCell();
 array_cell_1_1.sn = 'A1';
-array_cell_1_1.name = '名称';
+array_cell_1_1.name = '录入';
 array_cell_1_1.source_type = '1';
 array_cell_1_1.source_name = '录入';
 array_cell_1_1.source_data = '';
@@ -331,25 +331,25 @@ array_cell_1_1.source_sn = '';
 array_cell_1_1.row = '1';
 array_cell_1_1.col = 'A';
 
-export const array_cell_1_2 = new ArrayCell();
-array_cell_1_2.sn = 'B1';
-array_cell_1_2.name = '化学式';
-array_cell_1_2.source_type = '1';
-array_cell_1_2.source_name = '录入';
-array_cell_1_2.source_data = '';
-array_cell_1_2.source_sn = '';
-array_cell_1_2.row = '1';
-array_cell_1_2.col = 'B';
-
 export const array_cell_2_1 = new ArrayCell();
-array_cell_2_1.sn = 'A2';
-array_cell_2_1.name = '氯化钾';
+array_cell_2_1.sn = 'B1';
+array_cell_2_1.name = '化学式';
 array_cell_2_1.source_type = '1';
 array_cell_2_1.source_name = '录入';
 array_cell_2_1.source_data = '';
 array_cell_2_1.source_sn = '';
 array_cell_2_1.row = '2';
 array_cell_2_1.col = 'A';
+
+export const array_cell_1_2 = new ArrayCell();
+array_cell_1_2.sn = 'A2';
+array_cell_1_2.name = '引用';
+array_cell_1_2.source_type = '1';
+array_cell_1_2.source_name = '引用';
+array_cell_1_2.source_data = '';
+array_cell_1_2.source_sn = '';
+array_cell_1_2.row = '1';
+array_cell_1_2.col = 'B';
 
 export const array_cell_2_2 = new ArrayCell();
 array_cell_2_2.sn = 'B1';
@@ -361,56 +361,78 @@ array_cell_2_2.source_sn = '';
 array_cell_2_2.row = '2';
 array_cell_2_2.col = 'B';
 
-export const array_cell_3_1 = new ArrayCell();
-array_cell_3_1.sn = 'B1';
-array_cell_3_1.name = 'KCl';
-array_cell_3_1.source_type = '3';
-array_cell_3_1.source_name = '参数表';
-array_cell_3_1.source_data = '';
-array_cell_3_1.source_sn = '';
-array_cell_3_1.row = '3';
-array_cell_3_1.col = 'A';
+export const array_cell_1_3 = new ArrayCell();
+array_cell_1_3.sn = 'B1';
+array_cell_1_3.name = '日期';
+array_cell_1_3.source_type = '1';
+array_cell_1_3.source_name = '日期';
+array_cell_1_3.source_data = '';
+array_cell_1_3.source_sn = '';
+array_cell_1_3.row = '1';
+array_cell_1_3.col = 'C';
 
-export const array_cell_3_2 = new ArrayCell();
-array_cell_3_2.sn = 'B1';
-array_cell_3_2.name = 'KCl';
-array_cell_3_2.source_type = '4';
-array_cell_3_2.source_name = '公式';
-array_cell_3_2.source_data = '';
-array_cell_3_2.source_sn = '';
-array_cell_3_2.row = '3';
-array_cell_3_2.col = 'B';
+export const array_cell_2_3 = new ArrayCell();
+array_cell_2_3.sn = 'B1';
+array_cell_2_3.name = 'KCl';
+array_cell_2_3.source_type = '3';
+array_cell_2_3.source_name = '日期';
+array_cell_2_3.source_data = '';
+array_cell_2_3.source_sn = '';
+array_cell_2_3.row = '2';
+array_cell_2_3.col = 'C';
 
-export const array_cell_4_1 = new ArrayCell();
-array_cell_4_1.sn = 'B1';
-array_cell_4_1.name = 'KCl';
-array_cell_4_1.source_type = '5';
-array_cell_4_1.source_name = '录入';
-array_cell_4_1.source_data = '';
-array_cell_4_1.source_sn = '';
-array_cell_4_1.row = '4';
-array_cell_4_1.col = 'A';
+export const array_cell_1_4 = new ArrayCell();
+array_cell_1_4.sn = 'B1';
+array_cell_1_4.name = '参数表';
+array_cell_1_4.source_type = '1';
+array_cell_1_4.source_name = '参数表';
+array_cell_1_4.source_data = '';
+array_cell_1_4.source_sn = '';
+array_cell_1_4.row = '1';
+array_cell_1_4.col = 'D';
 
-export const array_cell_4_2 = new ArrayCell();
-array_cell_4_2.sn = 'B1';
-array_cell_4_2.name = 'KCl';
-array_cell_4_2.source_type = '1';
-array_cell_4_2.source_name = '录入';
-array_cell_4_2.source_data = '';
-array_cell_4_2.source_sn = '';
-array_cell_4_2.row = '4';
-array_cell_4_2.col = 'B';
+export const array_cell_2_4 = new ArrayCell();
+array_cell_2_4.sn = 'B1';
+array_cell_2_4.name = 'KCl';
+array_cell_2_4.source_type = '4';
+array_cell_2_4.source_name = '参数表';
+array_cell_2_4.source_data = '';
+array_cell_2_4.source_sn = '';
+array_cell_2_4.row = '2';
+array_cell_2_4.col = 'D';
 
-export const data_array = new DataArray(4, 2);
+export const array_cell_1_5 = new ArrayCell();
+array_cell_1_5.sn = 'B1';
+array_cell_1_5.name = '公式';
+array_cell_1_5.source_type = '1';
+array_cell_1_5.source_name = '公式';
+array_cell_1_5.source_data = '';
+array_cell_1_5.source_sn = '';
+array_cell_1_5.row = '1';
+array_cell_1_5.col = 'E';
+
+export const array_cell_2_5 = new ArrayCell();
+array_cell_2_5.sn = 'B1';
+array_cell_2_5.name = 'KCl';
+array_cell_2_5.source_type = '5';
+array_cell_2_5.source_name = '公式';
+array_cell_2_5.source_data = '';
+array_cell_2_5.source_sn = '';
+array_cell_2_5.row = '2';
+array_cell_2_5.col = 'E';
+
+export const data_array = new DataArray(2, 5);
 
 data_array.pushWithSpan(array_cell_1_1);
 data_array.pushWithSpan(array_cell_1_2);
+data_array.pushWithSpan(array_cell_1_3);
+data_array.pushWithSpan(array_cell_1_4);
+data_array.pushWithSpan(array_cell_1_5);
 data_array.pushWithSpan(array_cell_2_1);
 data_array.pushWithSpan(array_cell_2_2);
-data_array.pushWithSpan(array_cell_3_1);
-data_array.pushWithSpan(array_cell_3_2);
-data_array.pushWithSpan(array_cell_4_1);
-data_array.pushWithSpan(array_cell_4_2);
+data_array.pushWithSpan(array_cell_2_3);
+data_array.pushWithSpan(array_cell_2_4);
+data_array.pushWithSpan(array_cell_2_5);
 
 // 模块中的列表
 export const model_test = new Model();
