9b531ede00e3d60378e6ba6cd8c8a73730bca9b4
Hansien <hansien@live.com>
2018-03-04 21:31:40 +0800

    template preview

--- src/app/controller/sys_manage/man-template-preview.component.ts
+++ src/app/controller/sys_manage/man-template-preview.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnChanges } from '@angular/core';
 // import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { NgForOf, NgSwitch } from '@angular/common';
+import { NgForOf } from '@angular/common';
 import { OnInit } from '@angular/core';
 import 'rxjs/add/operator/finally';
 
@@ -9,18 +9,21 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { ModelDataService } from '../../service/model-data.service';
 // import { Model, Field, source } from '../../model/data-model'
-import { Model } from '../../model/data-model';
+import { Template, DataTable, source, DataArray } from '../../model/data-model';
+// import { DataManageService } from '../../service/data-manage.service';
 
 @Component({
     // tslint:disable-next-line:component-selector
-    selector: 'man-model-preview',
-    templateUrl: '../../view/man-model-preview.component.html',
+    selector: 'man-template-preview',
+    templateUrl: '../../view/man-template-preview.component.html',
     styleUrls: ['../../css/man-model-preview.component.css']
 })
-export class ManModelPreviewComponent {
-    @Input() model: Model;
-    // previewTemplate() { }
-    constructor(private modelDataService: ModelDataService) { }
+export class ManTemplatePreviewComponent {
+    @Input() template: Template;
+
+    constructor(private modelDataService: ModelDataService,) { }
+
+    previewTemplate() { }
 
     getKeys(item) {
         return this.modelDataService.getKeys(item);
