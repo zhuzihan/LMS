108956ca062ac3549710946cb47fa31c5d0afef5
yuchen <yuchenliu@live.com>
2018-03-05 10:50:58 +0800

    添加数组的数据来源

--- src/app/model/data-model.ts
+++ src/app/model/data-model.ts
@@ -68,6 +68,7 @@ export class DataArray {
     row_count = 0;
     rowspan = 0;
     colspan = 0;
+    source_sn = '';
     cell_list: Array<ArrayCell>;
     constructor(row, col) {
         this.row_count = row;
@@ -353,7 +354,7 @@ array_cell_1_2.row = '1';
 array_cell_1_2.col = 'B';
 
 export const array_cell_2_2 = new ArrayCell();
-array_cell_2_2.sn = 'B1';
+array_cell_2_2.sn = 'B2';
 array_cell_2_2.name = 'KCl';
 array_cell_2_2.source_type = '2';
 array_cell_2_2.source_name = '引用';
@@ -363,7 +364,7 @@ array_cell_2_2.row = '2';
 array_cell_2_2.col = 'B';
 
 export const array_cell_1_3 = new ArrayCell();
-array_cell_1_3.sn = 'B1';
+array_cell_1_3.sn = 'C1';
 array_cell_1_3.name = '日期';
 array_cell_1_3.source_type = '1';
 array_cell_1_3.source_name = '日期';
@@ -373,7 +374,7 @@ array_cell_1_3.row = '1';
 array_cell_1_3.col = 'C';
 
 export const array_cell_2_3 = new ArrayCell();
-array_cell_2_3.sn = 'B1';
+array_cell_2_3.sn = 'C2';
 array_cell_2_3.name = 'KCl';
 array_cell_2_3.source_type = '3';
 array_cell_2_3.source_name = '日期';
@@ -383,7 +384,7 @@ array_cell_2_3.row = '2';
 array_cell_2_3.col = 'C';
 
 export const array_cell_1_4 = new ArrayCell();
-array_cell_1_4.sn = 'B1';
+array_cell_1_4.sn = 'D1';
 array_cell_1_4.name = '参数表';
 array_cell_1_4.source_type = '1';
 array_cell_1_4.source_name = '参数表';
@@ -393,7 +394,7 @@ array_cell_1_4.row = '1';
 array_cell_1_4.col = 'D';
 
 export const array_cell_2_4 = new ArrayCell();
-array_cell_2_4.sn = 'B1';
+array_cell_2_4.sn = 'D2';
 array_cell_2_4.name = 'KCl';
 array_cell_2_4.source_type = '4';
 array_cell_2_4.source_name = '参数表';
@@ -403,7 +404,7 @@ array_cell_2_4.row = '2';
 array_cell_2_4.col = 'D';
 
 export const array_cell_1_5 = new ArrayCell();
-array_cell_1_5.sn = 'B1';
+array_cell_1_5.sn = 'E1';
 array_cell_1_5.name = '公式';
 array_cell_1_5.source_type = '1';
 array_cell_1_5.source_name = '公式';
@@ -413,7 +414,7 @@ array_cell_1_5.row = '1';
 array_cell_1_5.col = 'E';
 
 export const array_cell_2_5 = new ArrayCell();
-array_cell_2_5.sn = 'B1';
+array_cell_2_5.sn = 'E2';
 array_cell_2_5.name = 'KCl';
 array_cell_2_5.source_type = '5';
 array_cell_2_5.source_name = '公式';
@@ -434,6 +435,8 @@ data_array.pushWithSpan(array_cell_2_2);
 data_array.pushWithSpan(array_cell_2_3);
 data_array.pushWithSpan(array_cell_2_4);
 data_array.pushWithSpan(array_cell_2_5);
+
+data_array.source_sn = 'model.1#A1:A3#A1:A3';
 
 // 模块中的列表
 export const model_test = new Model();
