8862f200bbf89f803ba4cd79afb2baac5d1a8bcc
yuchen <yuchenliu@live.com>
2018-03-09 23:34:07 +0800

    修复cp命令的BUG，目测可以使用

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ if [ "$isUpdate" == "Already up-to-date."x ]; then
     echo "up-to-date"
 else
     ng build
-    cp -r ~/gitrepo/LMS/* /var/www/html/
+    cp -r ~/gitrepo/LMS/dist/* /var/www/html/
     # echo "up-to-date"
 fi
