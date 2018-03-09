beeea6b71ef8780e50af9ad2b5d26afe79f6ae54
yuchen <yuchenliu@live.com>
2018-03-09 23:30:34 +0800

    修复BUG，待测试

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 cd ~/gitrepo/LMS
 isUpdate=$(git pull)
 echo $isUpdate
-if [ $isUpdatex=="Already up-to-date."x ]; then
+if [ "$isUpdate" == "Already up-to-date."x ]; then
     echo "up-to-date"
 else
     ng build
