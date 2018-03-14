9a502f0e4b2cee9084a3927f7cbbc43ad0a6ae62
yuchen <yuchenliu@live.com>
2018-03-15 00:13:12 +0800

    修改paginator渲染时机

--- src/app/controller/sys_manage/man-equip-para-list.component.ts
+++ src/app/controller/sys_manage/man-equip-para-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, OnChanges,ViewChild } from '@angular/core';
+import { Component, OnInit, OnChanges, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatPaginator } from '@angular/material';
 import { ExpParameterService } from '../../service/exp-parameter.service';
+import { delay } from 'rxjs/operator/delay';
 // import {MatTableModule} from '@angular/material'
 
 @Component({
@@ -14,14 +15,14 @@ export class ManEquipParaListComponent implements OnInit, OnChanges {
     displayedColumns = ['tableName', 'tableRegistrant', 'tableRemark', 'tableState'];
     expParameterData: Array<Object>;
     expParameterList: Array<Object> = [];
-    dataSource = new MatTableDataSource;
-    
+    dataSource;
+
     @ViewChild(MatPaginator) paginator: MatPaginator;
 
     selectedExpParameter: Object;
     isLoading = true;
 
-    //new for test
+    // new for test
     Columns: Array<Object> = [];
     tableData: Array<Object> = [];
     tableKeys: Array<Object> = [];
@@ -30,18 +31,19 @@ export class ManEquipParaListComponent implements OnInit, OnChanges {
     constructor(private expParameterSerivce: ExpParameterService) { }
 
     ngOnInit(): void {
+        // this.isLoading = false;
         this.expParameterSerivce.getExpParameter().then(responseData => {
             this.expParameterData = responseData;
             this.expParameterList = this.expParameterSerivce.convertParameterList(this.expParameterData);
-            this.dataSource = new MatTableDataSource<Object>(this.expParameterList);
+            this.dataSource = new MatTableDataSource(this.expParameterList);
+            this.dataSource.paginator = this.paginator;
             this.isLoading = false;
             // console.log("afterngInit");
         });
-        this.dataSource.paginator = this.paginator;
     }
-    ngAfterViewInit() {
-        // this.dataSource.paginator = this.paginator;
-    } 
+    // ngAfterViewInit() {
+    //     this.dataSource.paginator = this.paginator;
+    // }
     ngOnChanges() {
         console.log(this.selectedExpParameter);
     }
