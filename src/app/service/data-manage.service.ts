5c7cf2de909cb6f5748145da307b91dd80ce2f46
yuchen <yuchenliu@live.com>
2018-04-04 11:43:40 +0800

    添加保存模块接口

--- src/app/service/data-manage.service.ts
+++ src/app/service/data-manage.service.ts
@@ -18,17 +18,28 @@ export class DataManageService {
     getOperationFlow(id: number): Promise<String> {
         const url = `${this.getDataUrl}`;
         return this.http.post(url, { 'id': id })
-                        .toPromise()
-                        .then(response => response.json().json as String)
-                        .catch(this.handleError);
+            .toPromise()
+            .then(response => response.json().json as String)
+            .catch(this.handleError);
     }
 
+    // 获取服务器模块列表
     getModules(): Promise<Array<Object>> {
         const url = `${this.getModuleUrl}`;
         return this.http.get(url)
-                        .toPromise()
-                        .then(response => response.json() as Array<Object>)
-                        .catch(this.handleError);
+            .toPromise()
+            .then(response => response.json() as Array<Object>)
+            .catch(this.handleError);
+    }
+
+    // 保存服务器模块列表
+    saveModules(name: string, registrant: string, state: Number, remark: string, json: string): Promise<Array<Object>> {
+        const url = `${this.getModuleUrl}`;
+        // tslint:disable-next-line:max-line-length
+        return this.http.post(url, { 'name': name, 'registrant': registrant, 'state': state, 'moduletype': 0, 'remark': remark, 'json': json })
+            .toPromise()
+            .then(response => response.json() as Array<Object>)
+            .catch(this.handleError);
     }
 
     private handleError(error: any): Promise<any> {
