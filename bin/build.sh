9cead5abd2e0c620e040bed28ef14b2798300df7
ubuntu <ubuntu@localhost.localdomain>
2018-04-10 00:45:40 +0800

    优化自动部署参数

--- bin/build.sh
+++ bin/build.sh
@@ -5,7 +5,7 @@ echo $isUpdate
 if [ "$isUpdate" == "Already up-to-date." ]; then
     echo "up-to-date"
 else
-    ng build
+    ng build --prod
     rm -rf /var/www/html/*
     cp -r ~/gitrepo/LMS/dist/* /var/www/html/
     # echo "up-to-date"
