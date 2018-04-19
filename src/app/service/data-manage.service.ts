c50421558b9b1e82798b5f32db03119abc6140ff
刘禹晨 <liuyuchen@liuyuchendeMacBook.local>
2018-04-19 16:19:34 +0800

    添加模板拉取和保存接口

--- src/app/service/data-manage.service.ts
+++ src/app/service/data-manage.service.ts
@@ -1,9 +1,7 @@
 // tslint:disable-next-line:eofline
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-
 import { baseUrl } from '../globals';
-
 import 'rxjs/add/operator/toPromise';
 
 // 从服务器获取模板数据, 采用Promise异步
@@ -11,7 +9,8 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class DataManageService {
     private getDataUrl = baseUrl + '/operationFlowById'; // 服务器地址
-    private getModuleUrl = baseUrl + '/module/'; // 获取模板 Api
+    private getModuleUrl = baseUrl + '/module/'; // 获取模块 Api
+    private getTemplateUrl = baseUrl + '/operationFlow/'; // 获取模板 Api
 
     constructor(private http: Http) { }
 
@@ -36,7 +35,29 @@ export class DataManageService {
     saveModules(name: string, registrant: string, state: Number, remark: string, json: string): Promise<Array<Object>> {
         const url = `${this.getModuleUrl}`;
         // tslint:disable-next-line:max-line-length
-        return this.http.post(url, { 'name': name, 'registrant': registrant, 'state': state, 'moduletype': 0, 'remark': remark, 'json': json })
+        const post_data = { 'name': name, 'registrant': registrant, 'state': state, 'moduletype': 0, 'remark': remark, 'json': json };
+        return this.http.post(url, post_data)
+            .toPromise()
+            .then(response => response.json() as Array<Object>)
+            .catch(this.handleError);
+    }
+
+    // 从服务器获取模板列表
+    getTemplates(): Promise<Array<Object>> {
+        const url = `${this.getTemplateUrl}`;
+        return this.http.get(url)
+            .toPromise()
+            .then(response => response.json() as Array<Object>)
+            .catch(this.handleError);
+    }
+
+    // 保存模板到服务器
+    // tslint:disable-next-line:max-line-length
+    saveTemplates(name: string, registrant: string, instructionbook: Number, remark: string, state: string, json: String): Promise<Array<Object>> {
+        const url = `${this.getTemplateUrl}`;
+        // tslint:disable-next-line:max-line-length
+        const post_data = { 'name': name, 'registrant': registrant, 'instructionbook': instructionbook, 'remark': remark, 'state': state, 'json': json};
+        return this.http.post(url, post_data)
             .toPromise()
             .then(response => response.json() as Array<Object>)
             .catch(this.handleError);
